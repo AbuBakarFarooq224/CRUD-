4bd47239a15d8cbea437da362752f50f090a0a22
unknown <abfarooq936@gmail.com>
2026-07-23 18:27:51 +0500

    root and health endpoints

---
+++ crud.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { get } = require('node:http');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get('/api', (req, res) => {
+  res.json({ name: 'Task API', version: '1.0', endpoints: ['/tasks'] });
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
